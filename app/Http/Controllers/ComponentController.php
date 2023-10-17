04aa72025160bfa134e6ac1003d121742b19d8b0
abazov73 <92822431+abazov73@users.noreply.github.com>
2023-10-17 20:41:13 +0400

    Added component index method

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SearchComponentRequest;
 use App\Http\Requests\StoreComponentRequest;
 use App\Http\Resources\ComponentResource;
 use App\Models\Characteristic;
@@ -11,12 +12,21 @@ use App\Services\ComponentService;
 use App\Services\ProtocolService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Config;
 
 class ComponentController extends Controller
 {
-    public function index()
+    public function index(SearchComponentRequest $request)
     {
-        
+        $queryString = $request->validated('search');
+        $numOfPages = $request->validated('perPage');
+
+        $components = Component::query()->where('name', 'LIKE', "%$queryString%")
+                                        ->orWhere('section_index', $queryString)
+                                        ->orWhere('id', $queryString)
+                                        ->paginate($numOfPages ?? config('constants.componentPerPageDefault'));
+
+        return ComponentResource::collection($components);
     }
 
     public function show(Component $component)
