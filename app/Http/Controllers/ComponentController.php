417753353b11bbd8c1a2072b1e82d9b35b3e3fd6
abazov73 <92822431+abazov73@users.noreply.github.com>
2023-10-17 20:08:51 +0400

    Added update component method

--- app/Http/Controllers/ComponentController.php
+++ app/Http/Controllers/ComponentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreComponentRequest;
+use App\Models\Characteristic;
 use App\Models\Component;
 use App\Services\CharacteristicService;
 use App\Services\ComponentService;
@@ -52,13 +53,42 @@ class ComponentController extends Controller
         return response('success');
     }
 
-    public function update(Component $component)
+    public function update(Component $component, StoreComponentRequest $request)
     {
+        $now = now();
+        $characteristics = [];
+        $protocols = [];
 
+        foreach ($request->validated('characteristics') as $characteristicData)
+        {
+            $characteristic = CharacteristicService::createWithoutSaving($characteristicData, $component->id);
+            foreach($characteristicData['protocols'] as $protocolsData)
+            {
+                $protocols[] = ProtocolService::createWithoutSaving($protocolsData, $characteristic->id);
+            }
+            $characteristics[] = $characteristic;
+        }
+
+        $component->characteristics->each(function (Characteristic $item, int $key) {
+            $item->protocols()->delete();
+        });
+        $component->characteristics()->where('created_at', '<', $now)->delete();
+
+        $component->update(Arr::except($request->validated(), ['characteristics']));
+
+        foreach ($characteristics as $characteristic)
+        {
+            $characteristic->save();
+        }
+        foreach ($protocols as $protocol)
+        {
+            $protocol->save();
+        }
     }
 
-    public function delete(Component $component)
+    public function destroy(Component $component)
     {
         ComponentService::destroy($component);
+        return response('success');
     }
 }
