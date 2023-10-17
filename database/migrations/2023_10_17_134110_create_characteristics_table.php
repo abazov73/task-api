0d2d34c18aaa46ef406eee8c8bd539f65ed8c3b8
abazov73 <92822431+abazov73@users.noreply.github.com>
2023-10-17 17:47:19 +0400

    Migrations for components, characteristics and protocols

---
+++ database/migrations/2023_10_17_134110_create_characteristics_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('characteristics', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->integer('account_count');
+            $table->string('ip');
+            $table->foreignId('component_id')->constrained('components')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('characteristics');
+    }
+};
---
+++ database/migrations/2023_10_17_134420_create_protocols_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('protocols', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['udp', 'tcp']);
+            $table->integer('port');
+            $table->foreignId('characteristic_id')->constrained('characteristics')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('protocols');
+    }
+};
