7efecc407ff614870a2728393b728611361a4078
abazov73 <92822431+abazov73@users.noreply.github.com>
2023-10-17 18:15:32 +0400

    Created routes and empty controller

---
+++ app/Http/Controllers/ComponentController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Component;
+use Illuminate\Http\Request;
+
+class ComponentController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function show(Component $component)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function update(Component $component)
+    {
+
+    }
+
+    public function delete(Component $component)
+    {
+
+    }
+}
